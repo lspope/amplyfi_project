f663619f949f4a203e13127c10087a8d68568c66
L. S. Pope <7350549+lspope@users.noreply.github.com>
2022-06-16 18:38:49 -0500

    Update article_exploration.py

--- code/article_exploration.py
+++ code/article_exploration.py
@@ -11,14 +11,21 @@ from streamlit import components
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
-
+st.set_page_config(layout="wide")
 st.title("Article Exploration Tool")
-
-DATE_COLUMN = "publishDate"
 
 @st.cache
 def load_sources():
     return mongoActions.get_sources()
+
+def get_metrics(df):
+     # get total articles, avg word count, min and max publishDates
+    total = df.shape[0]
+    min_yr = df["publishYear"].min()
+    max_yr = df["publishYear"].max()
+    date_range = "".join([str(min_yr), "-", str(max_yr)])
+    avg_wc = round(df["wordCount"].mean())
+    return (total, date_range, avg_wc)
 
 sources_list = load_sources()
 sources_nparray = np.array(sources_list)
@@ -36,22 +43,11 @@ if(st.button("Search by Source")):
     st.dataframe(source_filtered_df)
 
     # get total articles, avg word count, min and max publishDates
-    total_for_source = source_filtered_df.shape[0]
-    avg_wc_source = round(source_filtered_df["wordCount"].mean())
-    min_yr_source = source_filtered_df["publishYear"].min()
-    max_yr_source = source_filtered_df["publishYear"].max()
-    date_range_source = "".join([str(min_yr_source), "-", str(max_yr_source)])
-
-    col1, col2, col3 = st.columns(3)
-    col1.metric("Total Articles", total_for_source)
-    col2.metric("Covers", date_range_source)
-    col3.metric("Avg Word Count", avg_wc_source)
-
-    # get article counts for year
-    by_pub_year = source_filtered_df.groupby(["publishYear"])['id'].count()
-    for_plot = by_pub_year.reset_index()
-    for_plot.rename(columns={"publishYear": "Publish Year", "id": "Article Count"})
-    st.table(for_plot)
+    source_metrics = get_metrics(source_filtered_df)
+    sm_col1, sm_col2, sm_col3 = st.columns(3)
+    sm_col1.metric("Total Articles", source_metrics[0])
+    sm_col2.metric("Date Range", source_metrics[1])
+    sm_col3.metric("Avg Word Count", source_metrics[2])
 
     if len(source_filtered_df) > 3:
         # Get LDA topics from these articles
@@ -63,7 +59,6 @@ if(st.button("Search by Source")):
             with open('./lda.html', 'r') as f:
                 html_string = f.read()
                 components.v1.html(html_string, width=1200, height=800, scrolling=False)
-
 
 ##
 # Search by Entity and do LDA topic modeling on results
@@ -80,7 +75,14 @@ if(st.button("Search by Entity")):
         entity_filtered_df.drop(["body", "processedBodyWords", "PERSON", "NORP", "FAC", "ORG", "GPE", "LOC", "PRODUCT", "EVENT", "LAW"],
                                 axis=1, inplace=True)
         st.dataframe(entity_filtered_df)
-        
+
+        # get total articles, avg word count, min and max publishDates
+        entity_metrics = get_metrics(entity_filtered_df)
+        em_col1, em_col2, em_col3 = st.columns(3)
+        em_col1.metric("Total Articles", entity_metrics[0])
+        em_col2.metric("Date Range", entity_metrics[1])
+        em_col3.metric("Avg Word Count", entity_metrics[2])
+
         # Get LDA topics from these articles
         entity_lda_objs_tuple = nlpActions.get_lda_objects(entity_results_proc_texts)
 
@@ -105,6 +107,38 @@ if(st.button("Display Article")):
         st.text(f"Publish Date: {selected_doc.get('publishDate')}")
         st.text(f"Title: {selected_doc.get('title')}")
         st.text(f"Word Count: {selected_doc.get('wordCount')}")
+
         st.text(f"Entity Mentions: {selected_doc.get('entityMentions')}")
+
+        per_list = selected_doc.get("PERSON")
+        norp_list = selected_doc.get("NORP")
+        fac_list = selected_doc.get("FAC")
+        org_list = selected_doc.get("ORG")
+        gpe_list = selected_doc.get("GPE")
+        loc_list = selected_doc.get("LOC")
+        prod_list = selected_doc.get("PRODUCT")
+        event_list = selected_doc.get("EVENT")
+        law_list  = selected_doc.get("LAW")
+
+        if (len(per_list) > 0):
+            st.text(f"PERSON Entities: {selected_doc.get('PERSON')}")
+        if (len(norp_list) > 0):
+            st.text(f"NORP Entities: {selected_doc.get('NORP')}")
+        if (len(fac_list) > 0):
+            st.text(f"FAC Entities: {selected_doc.get('FAC')}")
+        if (len(org_list) > 0):
+            st.text(f"ORG Entities: {selected_doc.get('ORG')}")
+        if (len(gpe_list) > 0):
+            st.text(f"GPE Entities: {selected_doc.get('GPE')}")
+        if (len(loc_list) > 0):
+            st.text(f"LOC Entities: {selected_doc.get('LOC')}")
+        if (len(prod_list) > 0):
+            st.text(f"PRODUCT Entities: { selected_doc.get('PRODUCT')}")
+        if (len(event_list) > 0):
+            st.text(f"EVENT Entities: {selected_doc.get('EVENT')}")
+        if (len(law_list) > 0):
+            st.text(f"LAW Entities: {selected_doc.get('LAW')}")
+
         st.text_area("Summary", selected_doc.get('summary'))
         st.text_area("Body", selected_doc.get('body'))
+  
